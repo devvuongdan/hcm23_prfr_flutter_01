b9b4158253f711545646d40a435f179dfbacc221
VuongDan <dev.vuongdan@gmail.com>
2023-03-16 20:33:53 +0700

    [Trainer]: Fix prob 2

--- lib/pratice/problem_2_allowance.dart
+++ lib/pratice/problem_2_allowance.dart
@@ -3,30 +3,30 @@ import 'dart:io';
 void main() {
   // empty list
   final workerList = <Info>[];
-  int i = 0;
+  // int i = 0;
   // Info infoWorker = Info(name: '', salary: 0, grade: '');
   print("Enter the number of worker:");
   int? number = int.tryParse(stdin.readLineSync() ?? "0") ?? 0;
   for (int i = 0; i < number; i++) {
     print('Enter the information of worker');
-    // Info i = Info(name: '', salary: 0, grade: '');
-    workerList[i].inputInfo();
-    workerList.add(workerList[i]);
+    Info obj = Info(name: '', salary: 0, grade: '');
+    obj.inputInfo();
+    workerList.add(obj);
   }
 
-  for (int i = 0; i < number; i++) {
-    switch (workerList[i].grade) {
+  for (int j = 0; j < number; j++) {
+    switch (workerList[j].grade) {
       case "A":
-        workerList[i].salary = workerList[i].salary + 300;
+        workerList[j].salary = workerList[j].salary + 300;
         break;
       case "B":
-        workerList[i].salary = workerList[i].salary + 250;
+        workerList[j].salary = workerList[j].salary + 250;
         break;
       default:
-        workerList[i].salary = workerList[i].salary + 100;
+        workerList[j].salary = workerList[j].salary + 100;
         break;
     }
-    workerList[i].displayInfo();
+    workerList[j].displayInfo();
   }
 }
 

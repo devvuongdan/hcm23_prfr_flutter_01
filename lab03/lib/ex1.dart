4b95a85c3ea8b859c39af6a960edf61b53223c10
tdat3023 <tdat3023@gmail.com.com>
2023-03-18 21:18:38 +0700

    add navigation

--- lab03/lib/ex1.dart
+++ lab03/lib/ex1.dart
@@ -30,84 +30,68 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final ScrollController controller = ScrollController();
 
-  int _counter = 0;
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  void initState() {
-    controller.addListener(() {
-      print("Log ${controller.offset}");
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: const <Widget>[
-            Test(),
-            Test(),
-            Test(),
-            Test(),
-            Test(),
-            Test(),
-            Test(),
-            Test(),
-            Test(),
-            Test(),
-            Test(),
-            Test(),
-            Test(),
-            Test(),
-            Test(),
-            Test(),
-            Test(),
-            // thêm nhiều phần tử khác ở đây
+      body: Align(
+        alignment: Alignment.center,
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const Screen1()));
+              },
+              child: const Text('Press me'),
+            ),
+            const Text("test"),
           ],
-        ),
+        )),
       ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
 
-class Test extends StatelessWidget {
-  const Test({
-    super.key,
-  });
+class Screen1 extends StatefulWidget {
+  const Screen1({super.key});
+
+  @override
+  State<Screen1> createState() => _Screen1State();
+}
+
+class _Screen1State extends State<Screen1> {
+  final TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // margin: EdgeInsets.only(
-      //   top: 5,
-      //   bottom: 5,
-      //   left: (index == 0 ? 0 : 5),
-      //   right: (index == 49 ? 0 : 5),
-      // ),
-      height: 50,
-      width: 50,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.black,
-          width: 1.0,
-        ),
-        shape: BoxShape.circle,
-        color: Colors.amber,
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                labelText: "hello",
+                hintText: "This is hint",
+              ),
+              controller: controller,
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+
+                  //  Navigator.pushNamed(context, '/screen1', arguments: 'Hello from Screen 1');
+                },
+                child: const Text("Update and Back")),
+          ],
+        )),
       ),
     );
   }

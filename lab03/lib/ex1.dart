0962b139e31e45e2a5a1c28893a6c78a7883f82e
tdat3023 <tdat3023@gmail.com.com>
2023-03-18 21:53:46 +0700

    fix navigation

--- lab03/lib/ex1.dart
+++ lab03/lib/ex1.dart
@@ -29,6 +29,19 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final ScrollController controller = ScrollController();
+  String value = "ABC";
+
+  void update() {
+    setState(() {
+      value = "New Value";
+    });
+  }
+
+  void updateValueBy2ndSreeenParram(String newValue) {
+    setState(() {
+      value = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,13 +56,18 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => const Screen1()));
+              onPressed: () async {
+                // final String result =
+                await Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) =>
+                        Screen1(onUpdate: updateValueBy2ndSreeenParram)));
+                // setState(() {
+                //   value = result;
+                // });
               },
               child: const Text('Press me'),
             ),
-            const Text("test"),
+            Text(value),
           ],
         )),
       ),
@@ -58,7 +76,9 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class Screen1 extends StatefulWidget {
-  const Screen1({super.key});
+  // const Screen1({super.key});
+  final void Function(String) onUpdate;
+  const Screen1({super.key, required this.onUpdate});
 
   @override
   State<Screen1> createState() => _Screen1State();
@@ -85,8 +105,8 @@ class _Screen1State extends State<Screen1> {
             ),
             TextButton(
                 onPressed: () {
+                  widget.onUpdate.call(controller.text);
                   Navigator.of(context).pop();
-
                   //  Navigator.pushNamed(context, '/screen1', arguments: 'Hello from Screen 1');
                 },
                 child: const Text("Update and Back")),

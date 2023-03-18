17d3c531b8e1d68542c8c81f89841da1b76c8b6b
VuongDan <dev.vuongdan@gmail.com>
2023-03-18 21:38:36 +0700

    [Trainer]: Fix bai tap

---
+++ lab03/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/vuongdan/development/flutter_3.3.10"
+export "FLUTTER_APPLICATION_PATH=/Users/vuongdan/Desktop/R2SEDU/hcm23_prfr_flutter_01/lab03"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
